8d4f58fd68f4a4c8b1322fbaf6d0c1e2fae780aa
KeenoFourie <keenofourie614@gmail.com>
2023-05-17 10:12:37 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,19 @@
+// Question 1
+let person = {
+    firstName: 'Keeno',
+    age: 18
+};
+
+// Question 2
+let person2 = {
+    firstName: 'James',
+    age: 32
+};
+
+//  Question 3
+console.log(typeof person);
+console.log(typeof person2);
+
+// Question 4
+let data = [person, person2];
+console.log(data);
